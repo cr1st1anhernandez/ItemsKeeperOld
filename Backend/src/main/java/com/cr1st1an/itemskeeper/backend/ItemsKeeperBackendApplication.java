a998954baa7e3ae4be4b0f10558676fbb6df6e67
Cristian Hernandez Ramirez <cristian0402218@gmail.com>
2024-07-29 22:52:59 -0600

    🎉 Begin the project

---
+++ Backend/src/main/java/com/cr1st1an/itemskeeper/backend/ItemsKeeperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cr1st1an.itemskeeper.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsKeeperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsKeeperBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/cr1st1an/itemskeeper/backend/ItemsKeeperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cr1st1an.itemskeeper.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsKeeperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
